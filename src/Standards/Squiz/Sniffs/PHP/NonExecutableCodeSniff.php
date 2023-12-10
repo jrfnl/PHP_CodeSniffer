187f97427c74c1d32aa712417aa4643d182cd18b
jrfnl <jrfnl@users.noreply.github.com>
2023-12-10 03:35:59 +0100

    DO NOT PULL - see #149 | Squiz/NonExecutableCode: add extra defensive coding
    THIS SHOULD BE FIXED IN THE TOKENIZER INSTEAD.
    
    Includes unit test.

--- src/Standards/Squiz/Sniffs/PHP/NonExecutableCodeSniff.php
+++ src/Standards/Squiz/Sniffs/PHP/NonExecutableCodeSniff.php
@@ -109,7 +109,9 @@ class NonExecutableCodeSniff implements Sniff
             $next = $phpcsFile->findNext(Tokens::$emptyTokens, ($stackPtr + 1), null, true);
             if ($tokens[$next]['code'] === T_SEMICOLON) {
                 $next = $phpcsFile->findNext(Tokens::$emptyTokens, ($next + 1), null, true);
-                if ($tokens[$next]['code'] === T_CLOSE_CURLY_BRACKET) {
+                if ($tokens[$next]['code'] === T_CLOSE_CURLY_BRACKET
+                    && isset($tokens[$next]['scope_condition']) === true
+                ) {
                     // If this is the closing brace of a function
                     // then this return statement doesn't return anything
                     // and is not required anyway.
@@ -123,7 +125,7 @@ class NonExecutableCodeSniff implements Sniff
                     }
                 }
             }
-        }
+        }//end if
 
         if (isset($tokens[$stackPtr]['scope_opener']) === true) {
             $owner = $tokens[$stackPtr]['scope_condition'];
